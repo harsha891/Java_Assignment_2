16fbc693778217cb5a65b66d8fba920aad0efaa5
rajubehara0505 <rajubehara0505@gmail.com>
2025-03-05 00:02:51 -0500

    Part-2

--- src/main/java/com/term3/assignment2/Assignment_2.java
+++ src/main/java/com/term3/assignment2/Assignment_2.java
@@ -1,7 +1,41 @@
 package com.term3.assignment2;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
 public class Assignment_2 {
     public static void main(String arg[]) {
-        System.out.println("Hello, Welcome to Assignment 2");
+        ObjectMapper objMapper = new ObjectMapper();
+
+        List<Person> contributors = new ArrayList<>();
+        List<BlogPost> posts = new ArrayList<>();
+
+        try{
+            // Reading json files into lists
+            contributors = objMapper.readValue(new File("src/main/resources/person.json"), new TypeReference<>() {});
+            posts = objMapper.readValue(new File("src/main/resources/blogPosts.json"), new TypeReference<>() {});
+        }
+        catch (JsonMappingException e) {
+            System.out.println("Error : Invalid Json structure.");
+        }
+        catch (IOException e) {
+            System.out.println("Error : Issue with reading Json File.");
+        }
+
+        Blog blog = new Blog(posts, contributors);
+
+        // Print the total number of blog posts and contributors.
+        System.out.println("Total number of Blog post: " + posts.size());
+        System.out.println("Total number of Contributors: " + contributors.size());
+
+        // Calling getPostsByAuthorAge for author age 30
+        System.out.println("Posts of Author aged 30 - " + blog.getPostsByAuthorAge(30));
+
     }
 }
--- src/main/java/com/term3/assignment2/Blog.java
+++ src/main/java/com/term3/assignment2/Blog.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.ToString;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Getter
 @ToString
@@ -12,4 +14,21 @@ import java.util.List;
 public class Blog {
     private List<BlogPost> posts;
     private List<Person> contributors;
+
+    public Blog(List<BlogPost> posts, List<Person> contributors) {
+        this.posts = posts;
+        this.contributors = contributors;
+    }
+
+    public List<String> getPostsByAuthorAge(int age) {
+        // Created map of author Id and Age for post reference
+        Map<String, Integer> authorAgeMap = contributors.stream()
+                .collect(Collectors.toMap(Person::getId, Person::getAge));
+
+        return posts.stream()
+                .filter(post -> authorAgeMap.getOrDefault(post.getAuthorId(), -1) == age)
+                .map(BlogPost::getId)
+                .collect(Collectors.toList());
+    }
+
 }
